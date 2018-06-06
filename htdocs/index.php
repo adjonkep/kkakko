5b6615893b06bc6388270fd4a4077095888ef009
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-06-06 13:59:56 -0500

    more front

--- htdocs/index.php
+++ htdocs/index.php
@@ -40,7 +40,7 @@
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
           <a href="#" class="navbar-brand d-flex align-items-center">
-            <img class="mini-logo" src="img/kkakko_logo4.jpg" width="30" height="30"></img>
+            <strong>kkakko</strong>
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
