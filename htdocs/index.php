4a6e0a9629c4131d1b12279386a0e24a4ffc7a18
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-06-06 09:42:32 -0500

    more front-end

--- htdocs/index.php
+++ htdocs/index.php
@@ -7,7 +7,7 @@
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Album example for Bootstrap</title>
+    <title>Welcome to kkakko</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -54,11 +54,12 @@
 
       <section class="jumbotron text-center">
         <div class="container">
-          <h1 class="jumbotron-heading">Album example</h1>
-          <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+          <h1 class="jumbotron-heading">KKAKKO</h1>
+          <p class="lead text-muted">Bienvenue Sur KKaKKO, votre nuvelle facon d'expedier couriers et petits colis dans le monde a moindre cout.</p>
+          <p><h2>I want to Send a</h2></p>
           <p>
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+            <a href="#" class="btn btn-primary my-2">Parcel</a>
+            <a href="#" class="btn btn-secondary my-2">Courier</a>
           </p>
         </div>
       </section>
