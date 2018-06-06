5d39d299f29bbf49acc626573a4f2ea6a3c2f38e
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-06-06 13:47:38 -0500

    more front

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="icon" href="//kkakko.com/uploads/site_favicon.png">
 
-    <title>Welcome to kkakko</title>
+    <title>KkaKko - Reinventin Package Delivery</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
