62e4962b546f07e5fd55864417d73a551ed191fe
adrien djonkep <adjonkep@gmail.com>
2018-06-10 19:57:08 -0500

    DB connect

--- htdocs/index.php
+++ htdocs/index.php
@@ -144,18 +144,18 @@
           $username = "root";
           $password = "11QV8uzrYYar";
 
-          try {
-          $conn = new PDO("mysql:host=$servername;dbname=kkakko", $username, $password);
-          // set the PDO error mode to exception
-          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-          echo "Connected successfully"; 
-          $result = $conn->query("SELECT LastName FROM employee;");
-          }
-          catch(PDOException $e)
-          {
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=kkakko", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+            echo "Connected successfully"; 
+            $result = $conn->query("SELECT LastName FROM employee");
+            }
+        catch(PDOException $e)
+            {
             echo "Connection failed: " . $e->getMessage();
-          }
-?>
+            }
+        ?>
         <p>Designed by <?php echo $result; ?></p>
       </div>
     </footer>
