15d3cff61fe48528477e2f54b310d761fb23f61c
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-06-21 13:48:58 -0500

    align

--- htdocs/index.php
+++ htdocs/index.php
@@ -65,8 +65,6 @@
 
       <div class="album py-5 bg-light">
         <div class="container">
-
-          <span class="row">
             <div class="col-md-4">
               <div class="card mb-4 box-shadow">
                 <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
@@ -128,7 +126,6 @@
                 </div>
               </div>
             </div>
-          </span>
         </div>
       </div>
 
