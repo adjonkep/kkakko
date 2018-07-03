74ba8d7a69ec93464d7dde51ddedd969aa7f2677
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-07-03 15:21:04 -0500

    extra slashes

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -12,10 +12,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <title>KkaKko - Reinventing International Delivery</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="<?php echo base_url(); ?>/assets/css/kkakko.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/kkakko.css" rel="stylesheet">
   </head>
 
   <body>
@@ -157,7 +157,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-slim.min.js"><\/script>')</script>
     <script src="js/vendor/popper.min.js"></script>
-    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
     <script src="js/vendor/holder.min.js"></script>
   </body>
 </html>
