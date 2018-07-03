117cfd2c5ecdef54ee96712d9cf73c3d8295ef8e
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-07-03 14:01:12 -0500

    debug: added full path to assets-helper

--- application/helpers/assets_helper.php
+++ application/helpers/assets_helper.php
@@ -16,7 +16,7 @@ if ( ! function_exists('load_css'))
         $return = '';
         foreach ($css as $c)
         {
-            $filemtimeCSS = filemtime('assets/css/' . $c . '.css');
+            $filemtimeCSS = filemtime('/opt/bitnami/apps/kkakko/assets/css/' . $c . '.css');
             $return .= '<link rel="stylesheet" href="' . base_url() . 'assets/css/' . $c . '.css?v=' . $filemtimeCSS . '"/>' . "\n";
         }
         return $return;
@@ -37,7 +37,7 @@ if ( ! function_exists('load_js'))
         $return = '';
         foreach ($js as $j)
         {
-            $filemtimeJS = filemtime('assets/js/' . $j . '.js');
+            $filemtimeJS = filemtime('/opt/bitnami/apps/kkakko/assets/js/' . $j . '.js');
             $return .= '<script type="text/javascript" src="' . base_url() . 'assets/js/' . $j . '.js?v=' . $filemtimeJS . '"></script>' . "\n";
         }
         return $return;
