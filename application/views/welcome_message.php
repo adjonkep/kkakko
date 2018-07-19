0972c44466ef335ee2c16e13d74703ee29b8651c
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-07-19 14:58:17 -0500

    front

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -45,9 +45,9 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
           <p><a>The Pricing</a></p>
         </div>
         <div class="footer-icon">
-          <i class="fa fa-twitter-circle" title="" target="_blank" href="#"></i>
-          <i class="fa fa-facebook" title="" target="_blank" href="#"></i>
-          <i class="fab fa-google-plus-g"></i>
+          <i class="fab fa-twitter" href="#"></i>
+          <i class="fab fa-facebook" href="#"></i>
+          <i class="fab fa-google-plus-g" href="#"></i>
         </div>
         <div class="footer-right">
           <p><a>F.A.Q</a></p>
