82e2603300310e3fe8a10840a49a60453940e1d3
adrien djonkep <adjonkep@gmail.com>
2018-07-21 11:11:19 -0500

    front

--- application/views/send.php
+++ application/views/send.php
@@ -13,7 +13,8 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css"  >
-
+    <!-- Font Awesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/kkakko.css" >
   </head>
@@ -21,6 +22,17 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
   <body>
 
     <header>
+      <div class="topnav">
+        <div class="logo">
+          <a href="#">
+            <img src="<?php echo base_url(); ?>assets/images/kkakko-logos-13.png">
+          </a>
+        </div>
+        <div class="header-util">
+          <button class="w3-button">Login</button>
+          <button class="w3-button">Register</button>
+        </div>
+      </div>
     </header>
     <div id ="app">
         <h2>{{ product }} workflow.</h2>
@@ -39,7 +51,26 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
       </tr>     
      <?php }?>  
    </table>
-    <footer class="text-muted">
+    <footer>
+      <div class="footer-div">
+      <div class="footer-left">
+        <p><a>The Journey of kkakko</a></p>
+        <p><a>About Us</a></p>
+        <p><a>Contact Us</a></p>          
+        <p><a>The Contract</a></p>
+        <p><a>The Pricing</a></p>
+      </div>
+      <div class="footer-icon">
+        <i class="fab fa-twitter"><a href="http://twitter.kkakko.com"></a></i>
+        <i class="fab fa-facebook"><a href="http://facebook.kkakko.com"></a></i>
+        <i class="fab fa-linkedin"><a href="http://linkedin.kkakko.com"></a></i>
+      </div>
+      <div class="footer-right">
+        <p><a>F.A.Q</a></p>
+        <p><a>Privacy</a></p>
+        <p><a>Terms and Conditions</a></p>
+      </div>
+      </div>
     </footer>
 
     <!-- Bootstrap core JavaScript
@@ -47,9 +78,7 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="js/vendor/popper.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-    <script src="js/vendor/holder.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/vue.js"></script>
     <script>
       const app = new Vue({
