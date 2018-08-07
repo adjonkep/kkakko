69742326b6fbeb61640b53d53c4b117f0af8750e
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-08-07 10:40:45 -0500

    front

--- application/views/send.php
+++ application/views/send.php
@@ -267,7 +267,7 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
             },
             goBack: function(){
               vm.navigationStack[0].pop().hide();
-              vm.navigationStack[0].pop().show();
+              vm.navigationStack[0][this.vm.navigationStack.length -1].show();
               vm.navigationStack[1].pop().hide();
             }
           }
