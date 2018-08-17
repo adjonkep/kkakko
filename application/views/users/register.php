58eb3973bdd9b15fd7bc965598417743864c4ec7
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-08-17 13:35:44 -0500

    front

---
+++ application/views/users/register.php
@@ -0,0 +1,89 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="//kkakko.com/uploads/site_favicon.png">
+
+    <title>KkaKko - Reinventing International Delivery</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css"  >
+    <!-- Font Awesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/kkakko.css" >
+  </head>
+
+  <body>
+    <header>
+      <div class="topnav">
+        <div class="logo">
+          <a href="#">
+            <img src="<?php echo base_url(); ?>assets/images/kkakko-logos-13.png">
+          </a>
+        </div>
+      </div>
+    </header>
+       
+    <footer>
+      <div class="footer-div">
+      <div class="footer-left">
+        <p class="listing-left">
+          <a>The Journey of kkakko</a>
+          <a>About Us</a>
+          <a>Contact Us</a>          
+          <a>The Contract</a>
+          <a>The Pricing</a>
+        </p>
+      </div>
+      <div class="footer-icon">
+        <p>
+          <a href="http://twitter.kkakko.com"><i class="fab fa-twitter"></i></a>
+          <a href="http://facebook.kkakko.com"><i class="fab fa-facebook"></i></a>
+          <a href="http://linkedin.kkakko.com"><i class="fab fa-linkedin"></i></a>
+        </p>
+      </div>
+      <div class="footer-right">
+        <p class="listing-right">
+          <a>F.A.Q</a>
+          <a>Privacy</a>
+          <a>Terms and Conditions</a>
+        </p>
+      </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '240814613228994',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v3.1'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+  </body>
+</html>
