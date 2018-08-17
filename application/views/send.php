3f8e9ac91282c29fc06c41d93def79db07e718ec
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-08-17 11:00:46 -0500

    front

--- application/views/send.php
+++ application/views/send.php
@@ -311,7 +311,7 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
               }
             },
             loginStatus: function(){
-              var status = "<?php echo isset($_SESSION['name']);?>";
+              var status = "<?php echo $this->session->userdata('logged_in');?>";
               alert("The Login status is: " + status);
             }
           }
