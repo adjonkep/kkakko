347b34015ac3e994a6dbbf1d457f1bf4fabcd1f2
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-08-19 14:43:20 -0500

    backend

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -17,6 +17,11 @@
 			$this->load->model('LoginModel');
 			}
 
+		// Show registration page
+		public function user_registration_show() {
+			$this->load->view('users/register');
+		}
+
 		// Register user
 		public function register(){
 			$data['title'] = 'Sign Up';
@@ -26,11 +31,11 @@
 			$this->form_validation->set_rules('password', 'Password', 'required');
 			$this->form_validation->set_rules('password2', 'Confirm Password', 'matches[password]');
 			if($this->form_validation->run() === FALSE){
-				//$this->load->view('users/register', $data);
+				$this->load->view('users/register', $data);
 			} else {
 				// Encrypt password
 				$enc_password = md5($this->input->post('password'));
-				$this->user_model->register($enc_password);
+				//$this->user_model->register($enc_password);
 				// Set message
 				$this->session->set_flashdata('user_registered', 'You are now registered and can log in');
 				redirect('send');
