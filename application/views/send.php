3df7d93f2659951a798145f57729c0b9356768a4
adrien djonkep <adjonkep@gmail.com>
2018-08-24 23:02:44 -0500

    front

--- application/views/send.php
+++ application/views/send.php
@@ -330,7 +330,7 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
               }
             },
             loginStatus: function(){
-              alert("The Login status is: " <?php echo isset($_SESSION['logged_in']);?>);
+              alert("The Login status is: " + <?php echo isset($_SESSION['logged_in']);?>);
             }
           }
       })
