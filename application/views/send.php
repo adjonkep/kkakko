dc59cbd59fa691f07f21f6a8cec709ec951a9a5b
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-09-04 13:49:30 -0500

    register

--- application/views/send.php
+++ application/views/send.php
@@ -327,7 +327,7 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
                 alert("user logged in!");
               }
               else if(status == ""){
-                alert(<?php json_encode($sendInfo).concat(vm.sendingInfo); echo json_encode($sendInfo); ?>);
+                alert(<?php echo json_encode(array_merge($sendInfo, vm.sendingInfo); ?>);
                 window.location = "users/register";
               }
             }
