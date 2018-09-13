6b69173bc50432ed324b3a0f34cd5c70296ff9e6
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-09-13 16:05:16 -0500

    register

--- application/models/LoginModel.php
+++ application/models/LoginModel.php
@@ -1,5 +1,5 @@
 <?php
-Class LoginModel extends CI_Model {
+class LoginModel extends CI_Model {
 
 // Insert registration data in database
 public function registration_insert($data) {
@@ -14,13 +14,13 @@ $query = $this->db->get();
 if ($query->num_rows() == 0) {
 
 // Query to insert data in database
-$this->db->insert('user', $data);
-if ($this->db->affected_rows() > 0) {
-return true;
-}
+    $this->db->insert('user', $data);
+    if ($this->db->affected_rows() > 0) {
+    return true;
+    }
 } else {
-return false;
-}
+    return false;
+    }
 }
 
 // Read data using username and password
